7001d8aa2c737641da1b41e8ecdb325ff81684fe
MNisarAli <dr.nisaralig@gmail.com>
2023-06-09 20:09:16 +0500

    Create custom methods for the models as required

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  # Updates the posts counter for a user
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  # Returns the 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
