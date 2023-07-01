4dac7da5581af2b0b9c93b9e79609590fc36f388
MNisarAli <dr.nisaralig@gmail.com>
2023-07-01 08:45:09 +0500

    Fix issues in comments & likes controller

--- app/models/like.rb
+++ app/models/like.rb
@@ -8,6 +8,6 @@ class Like < ApplicationRecord
 
   # Updates the likes counter for a post
   def update_likes_counter
-    post.update(likes_counter: post.likes.count)
+    post.increment!(:likes_counter)
   end
 end
