ade40233ca33efade41f9c9d0a8b1425d84c237f
MNisarAli <dr.nisaralig@gmail.com>
2023-07-01 08:45:56 +0500

    Remove sleep period from test cases

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -6,13 +6,11 @@ RSpec.feature 'User Index Page', type: :feature do
     visit users_path
   end
   scenario 'I can see the username, profile picture, and number of posts for each user' do
-    sleep(2)
     expect(page).to have_content(@user1.name)
     expect(page.html).to include(@user1.photo)
     expect(page).to have_content(@user1.posts_counter)
   end
   scenario 'When I click on a user, I am redirected to their show page' do
-    sleep(2)
     click_on @user1.name
     expect(current_path).to eq(current_path)
   end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.describe 'User Show Page', type: :feature do
   describe 'Viewing user show page' do
     before(:each) do
-      @user1 = User.create(name: 'Tom', photo: 'https://icons.iconarchive.com/icons/iconsmind/outline/256/User-icon.png', bio: 'Teacher from Mexico.', posts_counter: 0)
+      @user1 = User.create(name: 'User 1', photo: 'https://icons.iconarchive.com/icons/iconsmind/outline/256/User-icon.png', bio: 'User from central America.', posts_counter: 0)
       @first_post = Post.create(author: @user1, title: 'post1', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
       @second_post = Post.create(author: @user1, title: 'post2', text: 'This is my second post', likes_counter: 0, comments_counter: 0)
       @third_post = Post.create(author: @user1, title: 'post3', text: 'This is my third post', likes_counter: 0, comments_counter: 0)
@@ -10,33 +10,26 @@ RSpec.describe 'User Show Page', type: :feature do
       visit "/users/#{@user1.id}"
     end
     it 'should show user clicked' do
-      sleep(2)
       expect(page).to have_content(@user1.name)
     end
     it 'should show user photo' do
-      sleep(2)
       expect(page.html).to include(@user1.photo)
     end
     it 'should show user bio' do
-      sleep(2)
       expect(page).to have_content(@user1.bio)
     end
     it 'should show posts_counter of clicked user' do
-      sleep(2)
       expect(page.html).to have_content(@user1.posts_counter)
     end
     it 'should show last three posts only' do
-      sleep(2)
       expect(page).to have_content(@fourth_post.title)
       expect(page).to have_content(@third_post.title)
       expect(page).to have_content(@second_post.title)
     end
     it 'should have a link to see all posts' do
-      sleep(2)
       expect(page.html).to include('See all posts')
     end
     it 'should show next_path when clicked on see all posts' do
-      sleep(2)
       click_on 'See all posts'
       expect(current_path).to eq(current_path)
     end
