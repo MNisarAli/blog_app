a7b31b57dad5ef2c24e7328d467d97cc7c582df9
MNisarAli <dr.nisaralig@gmail.com>
2023-09-20 18:52:49 +0500

    Add unit specs for user model methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe User, type: :model do
       expect(user.errors[:posts_counter]).to include('must be greater than or equal to 0')
     end
   end
+
+  context 'methods' do
+    user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300?img=58', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+
+    it 'returns the 3 most recent posts' do
+      posts = []
+      4.times do |i|
+        posts << Post.create(author: user, title: "Post#{i + 1}", text: "This is post#{i + 1}", comments_counter: 0,
+                             likes_counter: 0)
+      end
+
+      expect(user.recent_posts).to eq([posts.last, posts[-2], posts[-3]])
+    end
+  end
 end
