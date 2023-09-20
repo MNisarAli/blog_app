70f05c5b47d1df9b0e6f74cd925c28541675c353
MNisarAli <dr.nisaralig@gmail.com>
2023-09-20 17:43:36 +0500

    Add validation for user models name presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 50 }
+
   # Return the 3 most recent posts for a given user
   def recent_posts
     posts.order(created_at: :desc).limit(3)
