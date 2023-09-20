eb7da2b9cf6bc7b864da314c95978222c5f1fff1
MNisarAli <dr.nisaralig@gmail.com>
2023-09-20 18:02:53 +0500

    Add validation for likes counter to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { integer_only: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { integer_only: true, greater_than_or_equal_to: 0 }
 
   # Return the 5 most recent comments for a given post
   def recent_comments
