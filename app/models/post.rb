112dd7e6692b235cbf5e51b50b5118877f2709e4
MNisarAli <dr.nisaralig@gmail.com>
2023-09-20 18:00:41 +0500

    Add validation for title to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :title, presence: true, length: { maximum: 250 }
+
   # Return the 5 most recent comments for a given post
   def recent_comments
     comments.order(created_at: :desc).limit(5)
