e307da9d2b9559fb77c424ea1abc9824111051cd
MNisarAli <dr.nisaralig@gmail.com>
2023-09-20 17:45:05 +0500

    Add validation for post counter to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   # Return the 3 most recent posts for a given user
   def recent_posts
