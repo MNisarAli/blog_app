6aed0143581585f6fe1f5dc11d54a9ce74d7aca9
MNisarAli <dr.nisaralig@gmail.com>
2023-09-21 11:33:39 +0500

    Add unit specs for comment model methods

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # test goes here
+  context 'methods' do
+    user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300?img=58', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+    post = Post.create(author: user, title: 'My Post', text: 'This is a post', comments_counter: 0, likes_counter: 0)
+    comment = Comment.create(post:, author: user, text: 'This is a comment')
+
+    describe '#update_comments_counter' do
+      it 'does not update the comments counter if the post is not changed' do
+        expect { comment.update(text: 'Updated Comment') }.not_to(change { post.reload.likes_counter })
+      end
+    end
+  end
 end
