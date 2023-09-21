a25c511520469630ac6d091f04935a2040ad47c5
MNisarAli <dr.nisaralig@gmail.com>
2023-09-21 05:51:36 +0500

    Add unit specs for post model methods

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -30,4 +30,27 @@ RSpec.describe Post, type: :model do
       expect(post.errors[:likes_counter]).to include('must be greater than or equal to 0')
     end
   end
+
+  context 'methods' do
+    user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+    post = Post.create(author: user, title: 'My Post', text: 'This is a post', comments_counter: 0, likes_counter: 0)
+
+    describe '#recent_comments' do
+      it 'returns the 5 most recent comments' do
+        comments = []
+        6.times do |i|
+          comments << Comment.create(post:, author: user, text: "Comment #{i + 1}")
+        end
+
+        expect(post.recent_comments).to eq([comments[-1], comments[-2], comments[-3], comments[-4], comments[-5]])
+      end
+    end
+
+    describe '#update_posts_counter' do
+      it 'does not update the posts counter if the author is not changed' do
+        expect { post.update(text: 'Updated Text') }.not_to(change { user.reload.posts_counter })
+      end
+    end
+  end
 end
