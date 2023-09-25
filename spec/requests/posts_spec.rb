b006ce8026dbd5ec83276788d3bc43f110860c87
MNisarAli <dr.nisaralig@gmail.com>
2023-09-25 20:35:06 +0500

    Create controller for posts model with index & show actions

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
+      get '/posts/index'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      get '/posts/show'
       expect(response).to have_http_status(:success)
     end
   end
