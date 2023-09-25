b67dcb51d279a8c2aa275fefe91f9973971b6c2f
MNisarAli <dr.nisaralig@gmail.com>
2023-09-25 21:31:11 +0500

    Remove unnecessary single routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
