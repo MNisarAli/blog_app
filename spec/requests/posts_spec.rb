9bad8de2798ee9d5c447c33f8469edf0ffe7560f
MNisarAli <dr.nisaralig@gmail.com>
2023-09-26 14:40:11 +0500

    Write tests for index action in the posts_controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300?img=58', bio: 'Test user', posts_counter: 0)
+
   describe 'GET /index' do
+    before(:example) { get user_posts_path(user) }
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
