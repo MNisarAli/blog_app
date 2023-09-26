42b22c931c15d8962332b59cf149322a91542e92
MNisarAli <dr.nisaralig@gmail.com>
2023-09-26 14:06:18 +0500

    Write tests for get action in the users_contrller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:example) { get users_path }
+
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('List of all users')
     end
   end
 
