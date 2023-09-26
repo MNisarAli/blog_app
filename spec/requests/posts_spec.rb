30dc011a50f6d6ee985c34be0a3b46bd99a96050
MNisarAli <dr.nisaralig@gmail.com>
2023-09-26 14:45:38 +0500

    Write tests for show action in the posts_controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300?img=58', bio: 'Test user', posts_counter: 0)
+  post = Post.create(author: user, title: 'Test post', text: 'Test text', comments_counter: 1, likes_counter: 2)
 
   describe 'GET /index' do
     before(:example) { get user_posts_path(user) }
@@ -20,9 +21,18 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET /show' do
+    before(:example) { get user_post_path(user, post) }
+
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is the details of a specific post for a given user')
     end
   end
 end
