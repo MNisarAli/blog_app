bcf382fba2e6ae590cb22dd1995da3c56a8f7cb8
MNisarAli <dr.nisaralig@gmail.com>
2023-09-26 14:39:32 +0500

    Write tests for show action in the users_controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,9 +18,20 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET /show' do
+    user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300?img=58', bio: 'Test user', posts_counter: 0)
+
+    before(:example) { get user_path(user) }
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Here are the details for a selected user')
     end
   end
 end
