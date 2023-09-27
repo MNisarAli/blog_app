8620854af8b47036c304e7589041308cc3b0b80d
MNisarAli <dr.nisaralig@gmail.com>
2023-09-27 16:39:23 +0500

    Update post controller actions to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_posts_path(current_user, @post), notice: 'Post successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(comments_counter: 0, likes_counter: 0)
+  end
 end
