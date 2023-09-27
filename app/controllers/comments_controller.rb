29efaeca3a2782c7ae5b93b7e6518e66f4a0cf50
MNisarAli <dr.nisaralig@gmail.com>
2023-09-27 17:56:11 +0500

    Update comments controller actions to create new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: %i[new create]
+
   def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.author = current_user
+
+    render :new unless @comment.save
+
+    redirect_to user_post_path(user_id: @post.author.id, id: @post.id)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
