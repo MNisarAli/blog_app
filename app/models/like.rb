06ca75aa31706e895c4adc011580696d16362f46
MNisarAli <dr.nisaralig@gmail.com>
2023-09-28 13:03:26 +0500

    Add validation for likes author's uniqueness

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :author_id, uniqueness: { scope: :post_id }
+
   after_save :update_likes_counter
 
   private
