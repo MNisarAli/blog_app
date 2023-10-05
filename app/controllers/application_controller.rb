95c79cfeca11b6fed30e8207b72320a63d0c2113
MNisarAli <dr.nisaralig@gmail.com>
2023-10-05 15:02:28 +0500

    Update devise user model attributes in app ontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Add new attributes to the Devise User model
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name photo bio email password current_password])
+  end
 end
